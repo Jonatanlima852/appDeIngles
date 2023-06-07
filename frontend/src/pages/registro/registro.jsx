7ac6ed4c684efd7e91345586362c123c9c84cf70
Jonatan de Lima <jonatanlima852@gmail.com>
2023-06-07 09:26:01 -0300

    add tela de registro

---
+++ frontend/src/pages/registro/registro.jsx
@@ -0,0 +1,81 @@
+import React, {useState} from "react"
+import styles from './registro.module.css'
+import { useNavigate } from "react-router-dom"
+
+// const url = 'http://localhost:4000'
+
+
+
+
+
+export default function Registro(){
+    const navigate = useNavigate()
+
+    const id = 3
+
+    const [formData, setFormData] = useState({
+        email: '',
+        senha: '',
+    })
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    
+        // axios.post(`${url}/registrar`, formData)
+        //     .then(response => {
+        //         console.log('Enviado pae!')
+        //     })
+        //     .catch(err => {
+        //         console.log(err)
+        //     })
+
+        navigate(`/user/${id}/inicio`, {state: {id: id}})
+
+    }  
+    
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+    
+    
+    
+    return (
+        <div className={styles.telaForms}>
+            <form 
+                onSubmit = {handleSubmit}
+                className={styles.forms}>
+                <h1 className={styles.titulo}>Registrar</h1>
+                <input type="text" name="name" placeholder="Nome de usuário" 
+                    className={styles.input}
+                    value={formData.nome}
+                    onChange={handleChange}/>
+                <input type="text" name="email" placeholder="Email" 
+                    className={styles.input}
+                    value={formData.email}
+                    onChange={handleChange}/>
+                <input type="password" name="senha" placeholder="Senha" 
+                    className={styles.input}
+                    value={formData.senha}
+                    onChange={handleChange}/>
+                <input type="password" name="confirmarsenha" placeholder="Confirmar senha" 
+                    className={styles.input}
+                    value={formData.senha}
+                    onChange={handleChange}/>
+                <button
+                    className={styles.submeter}
+                    type="submit">
+                    Registrar
+                </button>
+                <div className={styles.texto}>
+                    <div 
+                        onClick={() => navigate('/login')}
+                        className={styles.clicavel}>
+                            Já possui login? Entre aqui!
+                    </div>
+                </div>
+            </form>
+        </div>    
+    )
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,7 @@
 import { Routes } from "react-router-dom"
 import PagInicial from "./pages/pagInicial/pagInicial"
 import Login from "./pages/login/login";
+import Registro from "./pages/registro/registro";
 import { Route } from 'react-router-dom';
 
 export default function RoutesComponent(){
@@ -8,6 +9,7 @@ export default function RoutesComponent(){
         <Routes>
             <Route exact path="/inicio" element={<PagInicial />} />
             <Route exact path="/login" element={<Login />} />
+            <Route exact path="/registro" element={<Registro />} />
         </Routes>
     )
 }
