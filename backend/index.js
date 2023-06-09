42de8ac0a633710bf6da58ec143f608c7af1d4bc
Jonatan de Lima <jonatanlima852@gmail.com>
2023-06-09 11:27:37 -0300

    update index.js on backend

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,16 @@ const app = require('express')()
 const consign = require('consign')
 
 
-cosign()
+consign()
+    .include('./config/database.js')
+    .then('./config/userModel.js')
     .then('./config/middlewares.js')
+    .then('./api/validation.js')
     .then('./api')
-    .then('./config/routes')
+    .then('./config/routes.js')
     .into(app)
+
+app.connectToDatabase(); 
 
 app.listen(4000, () => {
     console.log('Backend executando...')
