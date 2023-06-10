21921d15e02e10456910602d52bc980965b7f2f7
Jonatan de Lima <jonatanlima852@gmail.com>
2023-06-10 01:02:56 -0300

    fix englishTexts generator

--- backend/api/englishTexts.js
+++ backend/api/englishTexts.js
@@ -1,24 +1,20 @@
 // 37daf1fab93645a197fa366d18f8e759 - news api key
 
 module.exports = app => {
-
-
-    const getTextos = async (req, res) => {
+    const getTexts = async (req, res) => {
         const params = { ...req.body }
         // if(req.params.id) user.id = req.params.id
 
         const tema = params.tema ?? ''
-        const category = params.categoria ?? ''
-
+        console.log(req.body)
 
         key = '37daf1fab93645a197fa366d18f8e759'
         const NewsAPI = require('newsapi');
         const newsapi = new NewsAPI(key);
         // To query /v2/top-headlines
         // All options passed to topHeadlines are optional, but you need to include at least one of them
-        newsapi.v2.topHeadlines({
+        await newsapi.v2.topHeadlines({
             q: tema,
-            category: category,
             language: 'en',
             sortBy: 'relevancy',
         }).then(response => {
@@ -28,5 +24,5 @@ module.exports = app => {
         })
     }
 
-    return { getTextos }
+    return { getTexts }
 }
